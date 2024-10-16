29974a879879f70fe8c5a0b08d7c43eed863804f
Vineet Panchal <vineetpanchal03@gmail.com>
2024-10-15 22:33:48 -0400

    solved question 1768

---
+++ 1768.MergeStringsAlternatively/solution.py
@@ -0,0 +1,10 @@
+def mergeStringsAlternatively(word1, word2):
+  result = []
+  i = 0
+  while i < len(word1) or i < len(word2):
+    if i < len(word1):
+      result.append(word1[i])
+    if i < len(word2):
+      result.append(word2[i])
+    i += 1
+  return ''.join(result)
